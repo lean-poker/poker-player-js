6f4973f90770b6495a3d0666d3ad08da61044fe8
DeVill <devillmeister@gmail.com>
2015-10-28 07:44:17 +0100

    make it async

--- player.js
+++ player.js
@@ -3,8 +3,8 @@ module.exports = {
 
   VERSION: "Default JavaScript folding player",
 
-  bet_request: function(game_state) {
-    return 0;
+  bet_request: function(game_state, bet) {
+    bet(0);
   },
 
   showdown: function(game_state) {
