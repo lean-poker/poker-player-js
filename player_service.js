13638dd60efc16eccc96667209eae5dd3d8d2d4d
shadowgun@i.ua <shaman21027>
2016-07-13 09:51:20 +0300

    update express.js to 4.x; provide related changes

--- player_service.js
+++ player_service.js
@@ -1,9 +1,10 @@
 var player = require('./player');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
   res.send(200, 'OK')
